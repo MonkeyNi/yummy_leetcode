2f06836cbf17d459bf5527dd81257b69530f0016
niziyu <zyni@astri.org>
2021-03-07 15:23:20 +0800

    20210307

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,12 +1,28 @@
+from collections import defaultdict
+
+
 class Dijkstra:
     """
     Classic shortest path algorithm.
-    TODO: multi statisfied pathes
+    TODO: 
+        1. save shortest path
+        2. multi statisfied pathes
     """
 
-    def shortest_path(self, matrix):
+    def shortest_path(self, matrix, start=0):
+        """
+        Calculate the shortest path from start to each nodes
+
+        Args:
+            matrix ([list]): weight matrix
+            start (int, optional): [description]. Defaults to 0.
+
+        Returns:
+            [type]: [description]
+        """
         res = matrix[0]
-        visited = [0]
+
+        visited = [start]
         visiting = [i for i in range(len(matrix[0])) if not i in visited]
 
         while visiting:
@@ -24,11 +40,12 @@ class Dijkstra:
 
 
 test = Dijkstra()
-matrix = [[  0,  1,  12, 999, 999, 999],
+matrix = [
+ [0,  1,  12, 999, 999, 999],
  [999,   0,   9,   3, 999, 999],
  [999, 999,   0, 999,   5, 999],
  [999, 999,   4,   0,  13,  15],
  [999, 999, 999, 999,   0,   4],
  [999, 999, 999, 999, 999,   0]]
-res = test.shortest_path(matrix)
+res = test.shortest_path(matrix, start=1)
 print(res)
