fa8f3fa4d3defcb480be9f4f8b281a5be8b30155
nzy <niziyu2018@gmail.com>
2021-04-20 09:54:40 +0800

    20210420

--- DP/72_edit_distance.py
+++ DP/72_edit_distance.py
@@ -1,5 +1,8 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        """
+        Recursive method, from top to bottom (use memory to remove duplicate sub questions)
+        """
         if not word1:
             return len(word2)
         if not word2:
@@ -24,6 +27,29 @@ class Solution:
             return memo[(i, j)]
         
         return dp(len(word1)-1, len(word2)-1)
+
+    def minDistance_2(self, word1: str, word2: str) -> int:
+        """
+        dp table method, from bottom to top
+        """
+        m, n = len(word1), len(word2)
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = i
+        for i in range(n+1):
+            dp[0][i] = i
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(
+                        dp[i-1][j]+1,
+                        dp[i][j-1]+1,
+                        dp[i-1][j-1]+1
+                    )
+        return dp[m][n]
 
 test = Solution()
 word1 = "horse"
--- tmp.py
+++ tmp.py
@@ -1,45 +1,49 @@
-from typing import List
-
 class Solution:
-    # Time complexity: O(n^3)
-    #   still not the best, reduce it to O(n^2)
-    def stoneGameV(self, stoneValue: List[int]):
-        if len(stoneValue) <= 1:
-            return 0
-
-        n = len(stoneValue)
-        prefix = [0]*(n+1)
-        for i in range(n):
-            prefix[i+1] = prefix[i] + stoneValue[i]
-        dp_table = [[0 for _ in range(n+1)] for _ in range(n+1)]
-
-        def dp(i, j):
-            if i == j:
-                return 0
-            if dp_table[i][j] != 0:
-                return dp_table[i][j]
-            res = 0
-            for m in range(i, j):
-                left = prefix[m+1] - prefix[i]
-                right = prefix[j+1] - prefix[m+1]
-                if left <= right:
-                    res = max(res, dp(i, m) + left)
-                # ???
-                if left >= right:
-                    res = max(res, dp(m+1, j) + right)
-            dp_table[i][j] = res
-            return res
-        return dp(0, n-1)
+    def minDistance(self, word1: str, word2: str) -> int:
+        # m, n = len(word1), len(word2)
+        # dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        # for i in range(m+1):
+        #     dp[i][0] = i
+        # for i in range(n+1):
+        #     dp[0][i] = i
         
+        # for i in range(1, m+1):
+        #     for j in range(1, n+1):
+        #         if word1[i-1] == word2[j-1]:
+        #             dp[i][j] = dp[i-1][j-1]
+        #         else:
+        #             dp[i][j] = min(
+        #                 dp[i-1][j]+1,
+        #                 dp[i][j-1]+1,
+        #                 dp[i-1][j-1]+1
+        #             )
+        # return dp[m][n]
+        memo = {}
+        def help(i, j):
+            if i == -1:
+                return j+1
+            if j == -1:
+                return i+1
 
-
-
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if word1[i] == word2[j]:
+                memo[(i, j)] = help(i-1, j-1)
+                return help(i-1, j-1)
+            else:
+                memo[(i, j)] = min(
+                    help(i-1, j)+1,
+                    help(i-1, j-1) + 1,
+                    help(i, j-1)+1
+                )
+                return memo[(i, j)]
+        return help(len(word1)-1, len(word2)-1)
+    
+    
 test = Solution()
-stoneValue = [6,2,3,4,5,5]
-# stoneValue = [7,7,7,7,7,7,7]
-stoneValue = [2, 1, 1]
-res = test.stoneGameV(stoneValue)
+word1 = "horse"
+word2 = "ros"
+# word1 = "intention"
+# word2 = "execution"
+res = test.minDistance(word1, word2)
 print(res)
-        
-
-        
