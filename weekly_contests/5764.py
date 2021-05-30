c1a0e824f4f14bc072970fd9302c25d76192d007
niziyu <zyni@astri.org>
2021-05-30 14:30:12 +0800

    20210530

--- weekly_contests/5764.py
+++ weekly_contests/5764.py
@@ -1,23 +1,25 @@
 import math
 
+
 class Solution:
+    """
+    Typical binary search problem.
+    """
     def minSpeedOnTime(self, dist, hour):
         times = lambda s: sum(math.ceil(d / s) for d in dist[:-1])
         is_ontime = lambda s: times(s) + dist[-1] / s <= hour
-        base = times(1)
-        low, high = 1, base + math.ceil(dist[-1] / (hour - base))
-        # while low + 1 < high:
+        low, high = 1, 10**10
         while low < high:
             mid = low + (high - low) // 2
-            print(mid)
             if is_ontime(mid):
                 high = mid
             else:
                 low = mid + 1
-        return high if is_ontime(high) else -1
+        return low if is_ontime(low) else -1
+
 
 test = Solution()
 dist = [1, 3, 2]
-hour = 6
+hour = 2.7
 res = test.minSpeedOnTime(dist, hour)
 print(res)
