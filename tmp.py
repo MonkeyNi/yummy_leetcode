05af198e54a461ea591933ada73d26d27531c337
MonkeyNi <niziyu2018@gmail.com>
2021-10-20 17:32:21 +0800

    20211020

--- tmp.py
+++ tmp.py
@@ -1,17 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
-    def isPalindrome(self, head):
+    def addTwoNumbers(self, l1, l2):
         """
-        :type head: ListNode
-        :rtype: bool
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
         """
-        tmp = []
-        while head:
-            tmp.append(head.val)
+        
+        dummy = head = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            a = b = 0
+            if l1:
+                a = l1.val
+                l1 = l1.next
+            if l2:
+                b = l2.val
+                l2 = l2.next
+            carry, val = divmod(a+b+carry, 10)
+            head.next = ListNode(val)
             head = head.next
-        i = 0, j = len(tmp) - 1
-        while i < j:
-            if tmp[i] != tmp[j]:
-                return False
-            i += 1
-            j -= 1
-        return True
+        return dummy.next
